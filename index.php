427efa2cd7b9693d2fbceb2f1cdcdac5d9a7c0b7
EC2 Default User <ec2-user@ip-172-31-24-95.us-west-1.compute.internal>
2020-11-04 07:31:32 +0000

    update

--- index.php
+++ index.php
@@ -6,43 +6,47 @@
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">  
     </head>
+    
     <body> 
         <header>
-           <h1>Topic</h1>
+           <h1>Artificial Intelligence</h1>
         </header>
     <nav>
-        <a href="home.html"> Home </a>
-        <a href="page2.html"> Page2 </a>
-        <a href="page3.html"> Page3 </a>
-        <a href="page4.html"> Page4 </a>
+        <a href="index.html"> Home </a>
+        <a href="page2.html"> Types </a>
+        <a href="page3.html"> Subsets </a>
+        <a href="page4.html"> Future </a>
     </nav>
     
     <main>
         
         <figure>
-            <img src="img/hugo.jpeg" alt="Picture of Hugo Rodriguez" />        
+            <img src="img/artificial-intelligence-companies.jpg" alt="Picture of Artificial Intelligence" />        
         </figure>
         
         <div id="content">
 	        <br />
-			<p>Information regarding the topic. Information regarding the topic. Information regarding the topic. Information regarding the topic. Information regarding the topic.</p>
-			<p>I am a Software Engineer and have been recently hired at Acme.</p>
-			<p>Feel free to contact me!</p>
-			<br /><br />
-		    <em>"With ordinary talent and extraordinary <strong>perseverance</strong>, all things are attainable"</em><br />
-            - Thomas F. Buxton 	
+	        <p><strong>What is Artificial Intelligence?</strong></p>
+			<p>Artificial intelligence (AI) is a branch of computer science involving the creation of smart machines built to simulate human intelligence and perform tasks. 
+			   There are two types of AI: weak AI and strong AI. AI is an interdisciplinary science with multiple approaches, but advancements in machine learning and 
+			   deep learning are creating a paradigm shift in virtually every sector of the tech industry</p>
+			<br />
         </div>
         
-    
+       
+        
     </main>
 
     <footer>
          <hr>
+         <strong>Sources:</strong> 
+         <br>
+         <a href="https://builtin.com/artificial-intelligence">https://builtin.com/artificial-intelligence</a>
+         <br /> <br />
          CST336 Internet Programming. 2020&copy; Rodriguez <br />
          <strong>Disclaimer:</strong> The information in this webpage is fictitious. <br />
          It is used for academic purposes only.   
          <figure>
-          <img src="img/csumb-logo.jpg" alt="CSUMB logo" />
     </footer>    
     
     </body>
